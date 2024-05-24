05eae3bc4418411ea3a14f54417bc520787cd4bc
iamsatyapati <iamsatyapati@gmail.com>
2024-05-24 07:33:29 +0530

    json file reading using node

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const math = require('./math')
+// const math = require('./math')
 
-const { add, subtract } = math;
+// const { add, subtract } = math;
 
-console.log(add(4,2));
-console.log(subtract(4,2));
+// console.log(add(4,2));
+// console.log(subtract(4,2));
+
+const data = require('./data.json')  // require('./data') also give the same result
+
+console.log(data)
