37153dadf69d9e0318749041c37fba7d5b209cae
iamsatyapati <iamsatyapati@gmail.com>
2024-05-27 06:48:05 +0530

    buffer implementation done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-const PizzaShop = require('./pizza-shop')
-const DrinkMachine = require('./drink-machine')
+const buffer = new Buffer.from("Satya");
 
-const pizzaShop = new PizzaShop();
-const drinkMachine = new DrinkMachine();
-
-pizzaShop.on("order", (size, topping) => {
-    console.log(`Order received! Baking a ${size} pizza with ${topping}`);
-    drinkMachine.serveDrink(size);
-})
-
-pizzaShop.order("large", "mushrooms");
-pizzaShop.displayOrderNumber();
+console.log(buffer.toJSON());
+console.log(buffer);
+console.log(buffer.toString());
