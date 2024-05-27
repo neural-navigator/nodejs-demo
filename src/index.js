a605ad74d432fba7d1d80b3bb5ccaec0adf9ceee
iamsatyapati <iamsatyapati@gmail.com>
2024-05-27 07:01:55 +0530

    async and sync methods implemented on fs module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-const buffer = new Buffer.from("Satya");
+const { error } = require('node:console');
+const fs = require('node:fs')
 
-console.log(buffer.toJSON());
-console.log(buffer);
-console.log(buffer.toString());
+const fileContents = fs.readFileSync('./src/file.txt', 'utf-8')
+
+console.log(fileContents);
+
+console.log("Before async method");
+
+fs.readFile('./src/file.txt', 'utf-8', (error, data) => {
+    if(error){
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+})
+
+console.log("after async method");
+
+fs.writeFileSync('./src/file1.txt', "Hello world!")
+
+fs.writeFile('./src/file2.txt', 'Hello World!', {flag: "a"}, (error, data) => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("file written!");
+    }
+})
+
+// {flag: "a"} is used for append
